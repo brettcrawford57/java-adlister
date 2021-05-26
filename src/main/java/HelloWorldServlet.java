782d3c3240a6dc9a48142920545487753830f53a
Brett Crawford <brett.crawford57@gmail.com>
2021-05-26 12:39:53 -0500

    finished hello world servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.script.ScriptContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Locale;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    String name;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        name = req.getParameter("name");
+
+        if (name != null) {
+            res.getWriter().println("<h1>Hello, " + name + "!</h1>");
+        } else {
+            res.getWriter().println("<h1>Hello, World!</h1>");
+        }
 
     }
 }
+
+
